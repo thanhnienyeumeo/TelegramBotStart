3595682ca9856302088a248902d9dcbbd7d7b97b
NTHoang <nguyentronghoangmc@gmail.com>
2023-02-19 00:27:30 +0700

    1st version 19/2

--- main.py
+++ main.py
@@ -1,10 +1,55 @@
 import logging
 from telegram import Update
-from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
+from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes, MessageHandler, filters
 import os
-application = ApplicationBuilder().token(os.environ['TELETOKEN'])
+token = os.environ['TELETOKEN']
+print(token)
+application = ApplicationBuilder().token(os.environ['TELETOKEN']).build()
+
+def spottedUser(user):
+    print('Name:', user.name)
+    print('First name:', user.first_name)
+    print('Last name', user.last_name)      
+    print('Id:', user.id)
+    print('----------')
+
+def response(text: str):
+    if text in ['hello', 'hi', 'chào']:
+        return 'Lô con cặ*'
+    if 'Nam' in text:
+        return 
+    return "Chả hiểu gì sất"
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    spottedUser(update.effective_user)
     await context.bot.send_message(
-        chat_id = Update.effective_chat.id,
+        chat_id = update.effective_chat.id,
         text = 'Welcome to ChatGPTIT.'
     )
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    spottedUser(update.effective_user)
+    ans = ' '.join(context.args)
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = ans
+    )
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    spottedUser(update.effective_user)
+    ans = response(str(update.message.text).lower())
+
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = ans
+    )
+
+#add handle
+startHandler = CommandHandler('start', start)
+echoHandler = CommandHandler('echo', echo)
+messHandler = MessageHandler(filters.TEXT , handle_message)
+
+application.add_handler(startHandler)
+application.add_handler(echoHandler)
+application.add_handler(messHandler)
+print("Bot is ready")
+application.run_polling()
